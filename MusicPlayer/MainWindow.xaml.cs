68aa9bdad692fce3be316e6853d6e9d24394e858
RySiU111 <mkrychlik11@gmail.com>
2019-03-25 21:06:57 +0100

    AddedEventOnMouseOver

--- MusicPlayer/MainWindow.xaml.cs
+++ MusicPlayer/MainWindow.xaml.cs
@@ -24,5 +24,23 @@ namespace MusicPlayer
         {
             InitializeComponent();
         }
+
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            PrzyciskStop.Background = Brushes.Orange;
+            PrzyciskPlay.Background = Brushes.Orange;
+            PrzyciskPause.Background = Brushes.Orange;
+            PrzyciskNext.Background = Brushes.Orange;
+            PrzyciskPrevious.Background = Brushes.Orange;
+        }
+
+        private void Button_MouseLeave(object sender, MouseEventArgs e)
+        {
+            PrzyciskStop.Background = Brushes.White;
+            PrzyciskPlay.Background = Brushes.White;
+            PrzyciskPause.Background = Brushes.White;
+            PrzyciskNext.Background = Brushes.White;
+            PrzyciskPrevious.Background = Brushes.White;
+        }
     }
 }
